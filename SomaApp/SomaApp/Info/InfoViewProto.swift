148bdf41d932faea3d00f89b027a405ed2c09c6f
Michael Brockman <mbrockmandev@gmail.com>
2023-01-27 17:41:51 -0500

    add blur and card flipping for profiles

--- SomaApp/SomaApp/Info/InfoViewProto.swift
+++ SomaApp/SomaApp/Info/InfoViewProto.swift
@@ -9,30 +9,58 @@ import SwiftUI
 
 struct InfoViewProto: View {
   @State private var profiles = Profile.mock
+  @State var hasScrolled = false
   
   var body: some View {
-
-      NavigationStack {
-        
-
+    
+    ZStack {
+      Color.black.opacity(0.05)
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+      ScrollView {
+        scrollDetection
         ZStack {
-          TabView {
-                  ForEach(profiles, id: \.self) { profile in
-                    GeometryReader { proxy in
-                      ProfileViewProto(profile: profile)
-                        .padding()
-                    }
-                  }
-                }
-                .background {
-                  Image("soma_red_black")
-                }
-              .tabViewStyle(.page(indexDisplayMode: .never))
+          Image("soma_red_black")
+          featured
         }
-
-        
       }
+      .padding()
+      .shadow(radius: 20)
+    }
+    
   }
+  
+  var scrollDetection: some View {
+    GeometryReader { proxy in
+      Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+    }
+    .frame(height: 0)
+    .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+      withAnimation(.easeInOut) {
+        if value < 0 {
+          hasScrolled = true
+        } else {
+          hasScrolled = false
+        }
+      }
+    })
+  }
+  
+  var featured: some View {
+    TabView {
+      ForEach(profiles) { profile in
+        GeometryReader { proxy in
+          let minX = proxy.frame(in: .global).minX
+          ProfileViewProto(profile: profile)
+            .padding()
+            .rotation3DEffect(.degrees(minX / -10), axis: (x: 0, y: 1, z: 0))
+            .blur(radius: abs(minX / 100))
+        }
+      }
+    }
+    .tabViewStyle(.page(indexDisplayMode: .never))
+  }
+  
 }
 
 struct ProfileViewProto: View {
@@ -42,41 +70,37 @@ struct ProfileViewProto: View {
   
   var body: some View {
     
-      VStack(alignment: .center, spacing: 8) {
-        
-        RoundedRectangle(cornerRadius: 20)
-          .fill(.black.opacity(0.1))
-          .overlay(Image(profile.image)
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
-              .stroke(.linearGradient(colors: [.primary.opacity(0.3), .secondary.opacity(0.1)], startPoint: .bottom, endPoint: .top)))
-              .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.4)
-          )
-          .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
-          .padding([.top, .horizontal])
-          .onTapGesture {
-            isShowingDetail = true
-          }
-        Text(profile.belt.uppercased())
-          .font(.caption)
-          .foregroundColor(.secondary)
-          .lineLimit(1)
-          .padding(.leading)
-        Text(profile.name)
-          .fontWeight(.semibold)
-          .padding(.leading)
-      }
-      .padding(.bottom)
-      .frame(width: UIScreen.main.bounds.width * 0.9, height: UIScreen.main.bounds.height * 0.4)
-//      .frame(width: 350, height: sizeCategory > .large ? 480 : 360)
-      .background(.ultraThinMaterial)
-      .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
-      .shadow(radius: 20)
-      .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
-        .stroke(.linearGradient(colors: [.primary.opacity(0.3), .secondary.opacity(0.1)], startPoint: .bottom, endPoint: .top)))
-      .navigationDestination(isPresented: $isShowingDetail) {
-        InfoDetailView(profile: profile)
+    VStack(alignment: .center, spacing: 8) {
+      RoundedRectangle(cornerRadius: 20)
+        .fill(.black.opacity(0.1))
+        .overlay(Image(profile.image)
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
+            .stroke(.linearGradient(colors: [.primary.opacity(0.3), .secondary.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing)))
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.height * 0.3)
+        )
+        .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding([.top, .horizontal])
+        .onTapGesture {
+          isShowingDetail = true
+        }
+      Text(profile.belt.uppercased())
+        .font(.caption)
+        .foregroundColor(.secondary)
+        .lineLimit(1)
+        .padding(.leading)
+      Text(profile.name)
+        .fontWeight(.semibold)
+        .padding(.leading)
+    }
+    .padding(.bottom)
+    .background(.ultraThinMaterial)
+    .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+    .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
+      .stroke(.linearGradient(colors: [.primary.opacity(0.3), .secondary.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing)))
+    .popover(isPresented: $isShowingDetail) {
+      InfoDetailView(profile: profile)
     }
     
   }
