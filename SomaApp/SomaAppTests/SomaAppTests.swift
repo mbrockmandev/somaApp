8202331c490e8723206847fa0a203456ebc48b8f
Michael Brockman <mbrockmandev@gmail.com>
2023-01-28 09:08:46 -0500

    add unit tests structure

--- SomaApp/SomaAppTests/SomaAppTests.swift
+++ SomaApp/SomaAppTests/SomaAppTests.swift
@@ -1,36 +1,45 @@
-//
-//  SomaAppTests.swift
-//  SomaAppTests
-//
-//  Created by Michael Brockman on 1/19/23.
-//
+  //
+  //  SomaAppTests.swift
+  //  SomaAppTests
+  //
+  //  Created by Michael Brockman on 1/19/23.
+  //
 
 import XCTest
 @testable import SomaApp
+@testable import FirebaseCore
+@testable import FirebaseMessaging
 
 final class SomaAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  var sut: Model!
+  
+  override func setUpWithError() throws {
+    try super.setUpWithError()
+    sut = Model()
+  }
+  
+  override func tearDownWithError() throws {
+    sut = nil
+    try super.tearDownWithError()
+  }
+  
+  func getListingViewController() -> ListingViewController {
+    let controller = ListingViewController()
+    return controller
+  }
+  
+//  func testDownloadDataFromModel() {
+//
+//    let listingVC = getListingViewController()
+//    listingVC.model = sut
+//    sut.currentSubset = .ground
+//    sut.downloadData(for: .ground)
+//    dump(listingVC)
+//
+//    XCTAssertEqual(sut.guardVideos.count, 17)
+//  }
+//
+  
+  
 }
